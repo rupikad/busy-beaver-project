d280aa9501e78b7acfb18c39a455f26986721ecf
Rupika <rupika.d97@gmail.com>
2019-12-03 21:45:48 -0800

    indented & formatting

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,32 +1,30 @@
 <template>
- 
+
   <div>
-     <navbar></navbar>
+    <navbar></navbar>
     <div class="row">
       <div class="col-sm-4">
         <!--div class="container-fluid bg-1 text-left"-->
         <img
-          src="@/assets/main_pic.png"
-          class="img-responsive img-circle margin"
-          style="display:inline"
-          alt="Illustration: Character Searching for location"
-          width="400"
-          height="400"
-          align="right"
+                src="@/assets/main_pic.png"
+                class="img-responsive img-circle margin"
+                style="display:inline"
+                alt="Illustration: Character Searching for location"
+                width="400"
+                height="400"
+                align="right"
         />
         <!-- attribution https://icons8.com -->
       </div>
       <div class="lg col-md-8">
         <div class="sm col-sm-12" align="left">
-          <h2 class="highlight">What's a Busy Beaver?</h2>
+          <h2 class="highlight">What's the Busy Beaver anyway?</h2>
           <p></p>
           <p class="lead">"Missed your class because you were confused about the bus schedules"?</p>
-
-          <p
-            class="lead"
-          >"Lost because you are super new to the campus and not sure where your classes are at"?</p>
+          <p class="lead">"Lost because you are super new to the campus and not sure where your classes are at"?</p>
           <p class="lead">"Need an app that can help you be on time every time? "</p>
           <h5 class="display-5">The search ends here!</h5>
+
           <!-- <p>Busy Beaver helps you get to your location faster and way lot easier. It is convenience-on-the-go! With BusyBeaver, you can keep track of your class schedules, track bus timings, and search to explore locations quick.</p> -->
         </div>
         <!-- <div class="sm col-sm-12" align="left">
@@ -51,29 +49,29 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt augue interdum velit euismod in pellentesque massa. Sed nisi lacus sed viverra tellus in.</p>
       </div>
     </div> -->
-     <bbfooter></bbfooter>
+    <bbfooter></bbfooter>
   </div>
 </template>
 
 <script>
-import navbar from './Navbar.vue'
-import bbfooter from './Footer.vue'
+  import navbar from './Navbar.vue'
+  import bbfooter from './Footer.vue'
 
 
-export default {
-  name: "Main",
-  components:{
-    navbar,
-    bbfooter
-  }
-};
+  export default {
+    name: "Main",
+    components:{
+      navbar,
+      bbfooter
+    }
+  };
 </script>
 
 
 <style scoped>
-.row{
-padding:10px;
-}
+  .row{
+    padding:10px;
+  }
 
 
 </style>
