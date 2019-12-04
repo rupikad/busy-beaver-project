e0641621c6c8d51628761d5e61818e50a0b89d59
MadHatter01 <r.vaishnavi15@gmail.com>
2019-12-04 13:39:46 -0800

    Updated the Landing page with Content and CSS

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="curvature">
+  
 <nav class="navbar navbar-default" id="wave">
   <router-link to="/" ><h1>Busy Beaver </h1>  <p class="slab">Never miss a bus - Be on time, everytime!</p></router-link>
   <div class="nav-links">
@@ -11,7 +12,11 @@
      <router-link to="/map">Contact</router-link>
   </div>
 </nav>
+ <svg viewBox="0 4 500 50" preserveAspectRatio="none" >
+    <path d="M-79.34,-43.71 C114.78,130.95 207.72,-64.44 499.66,40.16 L500.00,0.00 L0.79,-2.27 Z" style="stroke: none;fill: #dc4405;"></path>
+  </svg>
 </div>
+
 </template>
 
 <script>
@@ -27,12 +32,15 @@ export default {
 .nav-links a{
   margin:0 40px;
 }
-
+path{
+  stroke: rgba(220, 68, 5,0.6)!important;
+  stroke-width: 8;
+}
 
 .navbar[data-v-beafdba2]{
   background-color: #dc4405;
   margin:0px;
-  padding:30px;
+  padding:10px 30px 0 30px;
 }
 .curvature{
   
